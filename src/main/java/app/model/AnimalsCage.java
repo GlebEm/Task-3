25bca2291ad73ea53dd0ddf90fadf1fa6f11ef66
GlebEm <gleb.emelyanov.2013@mail.ru>
2023-07-10 15:18:48 +0300

    Task2_Spring

--- src/main/java/app/model/AnimalsCage.java
+++ src/main/java/app/model/AnimalsCage.java
@@ -1,19 +1,31 @@
 package app.model;
 
+import app.config.AppConfig;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
-public class AnimalsCage {
+public class AnimalsCage  {
+//свяжите AnimalCage c бином Dog через абстрактный класс Animal.
 
     @Autowired
+    @Qualifier("Dog")
     private Animal animal;
+
+    @Autowired
+//    @Qualifier("Timer")
+    private Timer timer;
+
+    public Timer getTimer(){
+        return timer;
+    }
 
     public void whatAnimalSay() {
         System.out.println("Say:");
         System.out.println(animal.toString());
         System.out.println("At:");
-        System.out.println(new Timer().getTime());
+        System.out.println(timer.getTime());  //можно ли изменить тут newTimer на timer?
         System.out.println("________________________");
     }
 }
